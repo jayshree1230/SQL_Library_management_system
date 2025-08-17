de7ada8dac767b5554f13ca269513bfb54a97b09
Jayshree Suryawanshi <129727837+jayshree1230@users.noreply.github.com>
2025-08-17 16:58:34 +0530

    Update LibrabryManagementSystem.sql

--- LibrabryManagementSystem.sql
+++ LibrabryManagementSystem.sql
@@ -18,3 +18,79 @@ select * from members;
 select * from issued_status;
 delete from issued_status
 where issued_id='IS107';
+
+/*Q4 Retrive all the books issued by a specific employee where employee_id=E101*/
+select * from issued_status 
+where issued_emp_id="E101";
+
+/* Q5 List the member who have issued more than one book*/
+select issued_emp_id,
+count(issued_id) as Total_book_issued
+from issued_status
+group by issued_emp_id
+having count(issued_id)>1;
+
+/*Q6 create summary tables:used CTAS to generate new table based on query results -each book & total book_issued_cnt**/
+
+create table book_cnts
+as
+select b.isbn,
+count(ist.issued_id) as no_of_issued
+from books as b
+join 
+issued_status as ist
+on ist.issued_book_isbn=b.isbn
+group by 1;
+
+select * from book_cnts;
+
+/*Q7etrive all books in a specific category:Classic*/
+select * from books
+where category='Classic';
+
+/*Q8 Find Total Rental Income by each Category*/
+select b.category, 
+sum(b.rental_price) as Total_rental_price,
+count(*)
+from books as b
+join
+issued_status as ist
+on ist.issued_book_isbn=b.isbn
+group by category;
+
+/*Q9 list Members who registered in the Last 180 Days*/
+SELECT *
+FROM members
+WHERE reg_date >= CURDATE() - INTERVAL 180 DAY;
+
+insert into members(member_id,member_name,member_address,reg_date) 
+values
+('C188','sam','145 Main St','2025-16-08'),
+('C189','john','133 Main St','2025-11-08');
+
+/* Q10ist Employees with their Branch Managers name and their branch details*/
+select * from branch;
+select e1.emp_id,e1.emp_name,
+b.branch_id,
+e2.emp_name as manager
+from employees as e1
+join 
+branch as b 
+on b.branch_id=e1.branch_id
+join
+employees as e2
+on b.manager_id=e2.emp_id;
+
+/* Q11 create table of books with rental price above a certain threshold 7USD*/
+create table Books_price_greater_than_7
+select * from books
+where rental_price>7;
+select * from Books_price_greater_than_7;
+
+/*Q12 Retrive the list of books not yet Returned*/
+select 
+distinct ist.issued_book_name from issued_status as ist
+left join
+return_status as rs
+on rs.issued_id=ist.issued_id
+where rs.return_id is Null;
